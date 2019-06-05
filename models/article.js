ea73279f474c1ebbc2dd0349d236a8a1c1f36b7e
thuyngavo <lisa.vo49@gmail.com>
2019-06-04 19:01:13 -0500

    updated handlebars, js code for front end, etc.

--- models/article.js
+++ models/article.js
@@ -1,32 +1,27 @@
 var mongoose = require("mongoose");
 
-// Save a reference to the Schema constructor
 var Schema = mongoose.Schema;
 
-// Using the Schema constructor, create a new UserSchema object
-// This is similar to a Sequelize model
-var ArticleSchema = new Schema({
-  // `title` is required and of type String
-  title: {
-    type: String,
-    required: true
-  },
-  // `link` is required and of type String
-  link: {
-    type: String,
-    required: true
-  },
-  // `note` is an object that stores a Note id
-  // The ref property links the ObjectId to the Note model
-  // This allows us to populate the Article with an associated Note
-  note: {
-    type: Schema.Types.ObjectId,
-    ref: "Note"
-  }
+var articleSchema = new Schema({
+    article: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    url: {
+        type: String,
+        required: true
+    },
+    summary: {
+        type: String
+    },
+    date: String,
+    saved: {
+        type: Boolean,
+        default: false
+    }
 });
 
-// This creates our model from the above schema, using mongoose's model method
-var Article = mongoose.model("Article", ArticleSchema);
+var article = mongoose.model("article", articleSchema);
 
-// Export the Article model
-module.exports = Article;
+module.exports = article;
--- server.js
+++ server.js
@@ -1,45 +1,106 @@
-// Dependencies
-// =============================================================
-const express = require("express");
-const bodyParser = require("body-parser");
-const exphbs = require("express-handlebars");
-const cheerio = require("cheerio");
-const mongoose = require("mongoose");
-var logger = require("morgan");
-var axios = require("axios");
+// require dependencies
+var express = require("express");
+var mongoose = require("mongoose");
+var expressHandlebars = require("express-handlebars");
+var bodyParser = require("body-parser");
 
-var db = require("./models");
+// set up our port to be either the host's or our local port 3000
+var PORT = process.env.PORT || 3000;
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+// instantiate our express app
+var app = express();
 
-const router = express.Router();
+// set up an express router
+var router = express.Router();
 
-require(".config/routes")(router);
+// require our routes file pass our router object
+require("./config/routes")(router);
 
-app.engine("handlebars", exphbs({defaultLayout: "main"}));
+// designate public folder as a static directory
+app.use(express.static(__dirname + "/public"));
+
+// connect handlebars to our express app
+app.engine("handlebars", expressHandlebars({
+    defaultLayout: "main"
+}));
 app.set("view engine", "handlebars");
 
-app.use(express.static(__dirname + "public"));
+// use bodyparser in our app
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
+// have every request go through our router middleware
 app.use(router);
-const db = process.env.MONGODB_URI || "mongodb://localhost/unit18scraper";
+// If deployed, use deployed db else use local mongoHeadlines db
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect(db, function(err) {
-    if (err) {
-        console.log(err);
+// Connect mongoose to out database
+mongoose.connect(MONGODB_URI, function(error) {
+    //Log any errors connecting with mongoose
+    if (error) {
+        console.log(error);
     }
+    // or log a success message
     else {
-        console.log("connection successful");
+        console.log("mongoose connection is successful");
     }
 });
 
-// Starting our Express app
-// =============================================================
-app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
+// listen on the port
+app.listen(PORT, function() {
+    console.log("Listening on port: " + PORT);
 });
+
+
+
+// // Dependencies
+// // =============================================================
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const exphbs = require("express-handlebars");
+// const cheerio = require("cheerio");
+// const mongoose = require("mongoose");
+// var logger = require("morgan");
+// var axios = require("axios");
+
+// var db = require("./models");
+
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+
+// const router = express.Router();
+
+// require(".config/routes")(router);
+
+// app.engine("handlebars", exphbs({defaultLayout: "main"}));
+// app.set("view engine", "handlebars");
+
+// app.use(express.static(__dirname + "public"));
+
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.use(router);
+
+// // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// // mongoose.connect(MONGODB_URI);
+
+
+// mongoose.connect(MONGODB_URI, function(err) {
+//     if (err) {
+//         console.log(err);
+//     }
+//     else {
+//         console.log("connection successful");
+//     }
+// });
+
+// // Starting our Express app
+// // =============================================================
+// app.listen(PORT, function () {
+//     console.log("App listening on PORT " + PORT);
+// });
