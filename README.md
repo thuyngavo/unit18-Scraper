8c0d23227a4ad6da17d33d3107676a1a90a9769c
thuyngavo <lisa.vo49@gmail.com>
2019-06-04 20:34:41 -0500

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # unit18-News Scraper
+Scrape articles you are interested in and save them for later. 
+Visit the deployed application: 
+
+#Contributors
+@thuyngavo
+
+# About
+This application allows users scrape, save and leave  comments. Users should also be able to delete comments left on articles.
+
+# Contact
+- Homepage: https://github.com/thuyngavo/thuyngavo.github.io
+- e-mail: lisa.vo49@gmail.com
+- LinkedIn: https://www.linkedin.com/in/lisa-vo-86a94341/
--- server.js
+++ server.js
@@ -1,106 +1,62 @@
-// require dependencies
+// Dependencies
+//====================================================
 var express = require("express");
 var mongoose = require("mongoose");
 var expressHandlebars = require("express-handlebars");
 var bodyParser = require("body-parser");
+var cheerio = require ("cheerio");
 
-// set up our port to be either the host's or our local port 3000
+// Port
+//====================================================
 var PORT = process.env.PORT || 3000;
 
-// instantiate our express app
+// Initiate express
+//====================================================
 var app = express();
 
-// set up an express router
+// Express router
+//====================================================
 var router = express.Router();
 
-// require our routes file pass our router object
+// Route
+//====================================================
 require("./config/routes")(router);
 
-// designate public folder as a static directory
 app.use(express.static(__dirname + "/public"));
 
-// connect handlebars to our express app
+// Handlebars views
+//====================================================
 app.engine("handlebars", expressHandlebars({
     defaultLayout: "main"
 }));
 app.set("view engine", "handlebars");
 
-// use bodyparser in our app
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-// have every request go through our router middleware
+// Middleware
+//====================================================
 app.use(router);
+
+
 // If deployed, use deployed db else use local mongoHeadlines db
+//====================================================
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-// Connect mongoose to out database
-mongoose.connect(MONGODB_URI, function(error) {
-    //Log any errors connecting with mongoose
-    if (error) {
-        console.log(error);
+// Connect mongoose
+//====================================================
+mongoose.connect(MONGODB_URI, function(err) {
+    if (err) {
+        console.log(err);
     }
-    // or log a success message
     else {
-        console.log("mongoose connection is successful");
+        console.log("connection is successful");
     }
 });
 
-// listen on the port
+// Listener
+//====================================================
 app.listen(PORT, function() {
     console.log("Listening on port: " + PORT);
 });
-
-
-
-// // Dependencies
-// // =============================================================
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const exphbs = require("express-handlebars");
-// const cheerio = require("cheerio");
-// const mongoose = require("mongoose");
-// var logger = require("morgan");
-// var axios = require("axios");
-
-// var db = require("./models");
-
-// const app = express();
-// const PORT = process.env.PORT || 3000;
-
-// const router = express.Router();
-
-// require(".config/routes")(router);
-
-// app.engine("handlebars", exphbs({defaultLayout: "main"}));
-// app.set("view engine", "handlebars");
-
-// app.use(express.static(__dirname + "public"));
-
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-
-// app.use(router);
-
-// // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
-// // mongoose.connect(MONGODB_URI);
-
-
-// mongoose.connect(MONGODB_URI, function(err) {
-//     if (err) {
-//         console.log(err);
-//     }
-//     else {
-//         console.log("connection successful");
-//     }
-// });
-
-// // Starting our Express app
-// // =============================================================
-// app.listen(PORT, function () {
-//     console.log("App listening on PORT " + PORT);
-// });
