1020545b20e3cce792e15c11513882de80e7f00a
thuyngavo <lisa.vo49@gmail.com>
2019-06-20 16:07:53 -0500

    correcting issues with page

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,112 +1,73 @@
-//==================================================================
-//Render articles to page
-//==================================================================
+//=======================================================
+// Grab the articles as a json
+//=======================================================
 
-//variable to indicate container for rendering articles
-var articleContainer = $(".article-container");
-
-// empty the article container
-articleContainer.empty();
-
-
-//function to render articles to page
-$.get("/articles").then(function(data) {
-  //loop through each article in database
+$.getJSON("/articles", function (data) {
+  // For each one
   for (var i = 0; i < data.length; i++) {
-    //if there are articles
-    if (data && data.length) {
-    //render then to the page
-    renderArticles(data);
-    // $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-  } else {
-    //else render empty alert
-    renderEmpty();
-}
+    // Display the apropos information on the page
+    $("#articles").append(
+      "<div class='container'>" +
+      "<h2 data-id='" + data[i]._id + "' data-toggle='modal' data-target='#exampleModal'>" + data[i].title + "</h2>" +
+      "<p data-id='" + data[i]._id + "'>"
+      + data[i].link + "<br />"
+      + data[i].summary + "</p>" +
+      "<button type='button' class='btn btn-danger save'data-id='" + data[i]._id + "'> "+
+      "Save Article" +
+      "</button>"+ "   " +
+      "<button type='button' class='btn btn-primary save'data-id='" + data[i]._id + "'> "+
+      "Delete Article" +
+      "</button>"+
+      "</div>" + "<br><br>"
+    );
   }
 });
 
-//function to render articles
-function renderArticles(data) {
-  //variable to hold empty array
-  var articlePanels = [];
-  // loop through articles data and push to article panels array
+//=======================================================
+// Grab the saved articles as a json
+//=======================================================
+
+$.getJSON("/articles", function (data) {
+  // For each one
   for (var i = 0; i < data.length; i++) {
-      articlePanels.push(createPanel(data[i]));
-  }
-  // once we have all of the HTML for the articles stored in our articlePanels array, append them to the articlePanels container
-  articleContainer.append(articlePanels);
-}
-
-//function to create article cards
-function createPanel(data) {
-  //variable to print articles in card
-  var panel = 
-    $(["<div class='card' style='width: 18rem;'>",
-          "<div class='card-body'>",
-            "<h5 class='card-title'>", data.title, "</h5>",
-            "<p class='card-text'>", data.summary ,"</p>",
-            "<p class='card-text'>", data.summary ,"</p>",
-            "<a href='#' class='card-link'>", data.link, "</a>",
-            "<a href='#' class='card-link delete btn.delete'>Delete</a>",
-            "<a href='#' class='card-link readLink notes'>Notes</a>",
-          "</div>",
-        "</div>"   
-      ].join("")
+    // Display the apropos information on the page
+    $("#savedArticles").append(
+      "<div class='container'>" +
+      "<h2 data-id='" + data[i]._id + "' data-toggle='modal' data-target='#exampleModal'>" + data[i].title + "</h2>" +
+      "<p data-id='" + data[i]._id + "'>"
+      + data[i].link + "<br />"
+      + data[i].summary + "</p>" +
+      "<button type='button' class='btn btn-primary delete'data-id='" + data[i]._id + "'> "+
+      "Delete Article" +
+      "</button>"+
+      "</div>" + "<br><br>"
     );
-  //attach the article's id
-  panel.data("_id", article._id);
-  //return panel element
-  return panel;
-}
+  }
+});
 
-//function to render an emtpy article container
-function renderEmpty() {
-  //variable to print that there are no articles
-  var emptyAlert =
-  $([`<div class='alert alert-warning text-center'>
-        <h4>No articles saved...</h4>
-      </div>
-      <div class='panel panel-default'>
-        <div class='panel-heading text-center'>
-          <h4>Lets go back and scrape some articles.</h4>
-        </div>
-        <div class='panel-body text-center'>
-          <h4><a href='/'>BACK</a></h4>
-        </div>
-      </div>`
-    ].join("")
-  );
-  //render alert html to page
-  articleContainer.append(emptyAlert);
-}
-
-//==================================================================
-//click function to delete article
-//==================================================================
-
-//onclick function to delete saved article
-$(document).on("click", ".btn.delete", function(){
-  //variable to grab article user selects to delete
-  var articleToDelete = $(this).parents(".panel").data();
-
-  //ajax call to initiate delete from db
+//=======================================================
+//DELETE: delete an article user selected
+//=======================================================
+$(document).on("click", ".delete", function () {
+  // Save the id from the button
+  var thisId = $(this).attr("data-id");
+  // Now make an ajax call 
   $.ajax({
     method: "DELETE",
-    url: "/api/title/" + articleToDelete._id
-  }).then(function(data) {
-    //reload page 
-    if (data.ok) {
-        location.reload();
-    }
-});
+    url: "/articles/" + thisId
+  })
+    // With that done, add the note information to the page
+    .then(function (data) {
+      res.json(data);
+    });
 });
 
-//==================================================================
-//click functions for notes
-//==================================================================
+//=======================================================
+//GET: read a note (if any) on an article user selected
+//=======================================================
 
-//onclick function for note button
-$(document).on("click", ".btn.notes", function() {
+// Whenever someone clicks a h2 tag
+$(document).on("click", "h2", function () {
   // Empty the notes from the note section
   $("#notes").empty();
   // Save the id from the p tag
@@ -118,7 +79,7 @@ $(document).on("click", ".btn.notes", function() {
     url: "/articles/" + thisId
   })
     // With that done, add the note information to the page
-    .then(function(data) {
+    .then(function (data) {
       console.log(data);
       // The title of the article
       $("#notes").append("<h2>" + data.title + "</h2>");
@@ -127,7 +88,10 @@ $(document).on("click", ".btn.notes", function() {
       // A textarea to add a new note body
       $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
       // A button to submit a new note, with the id of the article saved to it
-      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+      $("#notes").append("<button type='button' class='btn btn-secondary' data-dismiss='modal' data-id='" + data._id + "' id='savenote'>Save</button>");
+      // A button to close modal
+      $("#notes").append("<button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>");
+      
 
       // If there's a note in the article
       if (data.note) {
@@ -139,12 +103,13 @@ $(document).on("click", ".btn.notes", function() {
     });
 });
 
-//==================================================================
-//click function to edit notes
-//==================================================================
+//=======================================================
+//POST: edit note on an article
+//=======================================================
 
-//on click
-$(document).on("click", ".btn.save", function() {
+
+// When you click the savenote button
+$(document).on("click", "#savenote", function () {
   // Grab the id associated with the article from the submit button
   var thisId = $(this).attr("data-id");
 
@@ -160,7 +125,7 @@ $(document).on("click", ".btn.save", function() {
     }
   })
     // With that done
-    .then(function(data) {
+    .then(function (data) {
       // Log the response
       console.log(data);
       // Empty the notes section
@@ -172,172 +137,7 @@ $(document).on("click", ".btn.save", function() {
   $("#bodyinput").val("");
 });
 
-
-//==================================================================
-//click function to save notes
-//==================================================================
-
-// //on click
-// $(document).on("click", ".btn.save", function() {
-//   // variable s to grab user input
-//   var noteData;
-//   var newNote = $(".bootbox-body textarea").val().trim();
-
-//   //if there is new input
-//   if (newNote) {
-//       //change current note to new input
-//       noteData = {
-//           _id: $(this).data("article")._id,
-//           noteText: newNote
-//       };
-
-//       $.post("/api/notes", noteData).then(function() {
-//           //close the modal
-//           bootbox.hideAll();
-//       });
-//   }
-// });
-
-//==================================================================
-//click function to delete notes
-//==================================================================
-
-//on click
-$(document).on("click", ".btn.note-delete" , function() {
-
-    //variable to grab id of note selected
-    var noteToDelete = $(this).data("_id");
-
-    //ajax call to delete from db
-    $.ajax({
-      url: "/api/notes/" + noteToDelete,
-      method: "DELETE"
-  }).then(function() {
-      //hide the modal
-      bootbox.hideAll();
-  });
+// When you click the savenote button
+$(document).on("click", "#close", function () {
+  console.log("No Change");
 });
-
-
-
-
-
-
-
-
-
-
-
-// /* global bootbox */
-// $(document).ready(function() {
-//   // setting class article-container div where all the content will go
-//   // adding event listeners to any dynamically generated "save article" & "scrape new article" buttons
-//   var articleContainer = $(".article-container");
-//   $(document).on("click", ".btn.save", handleArticleSave);
-//   $(document).on("click", ".scrape-new", handleArticleScrape);
-
-//   // once the page is ready, run the initPage function to kick things off
-//   initPage();
-  
-//   function initPage() {
-//       // empty the article container, run an AJAX request for any unsaved headlines
-//       articleContainer.empty();
-//       $.get("/api/headlines?saved=false")
-//         .then(function(data) {
-//             // if we have headlines, render them to the page
-//             if (data && data.length) {
-//                 renderArticles(data);
-//             } else {
-//                 // otherwise render a message explaining we have no articles
-//                 renderEmpty();
-//             }
-//           });
-//       }
-
-//       function renderArticles(articles) {
-//           // this function handles appending html containing our article data to the page with an array of JSON with all available articles
-//           var articlePanels = [];
-//           // pass each article JSON object to the createPanel function which returns a bootstrap panel with article data inside
-//           for (var i = 0; i < articles.length; i++) {
-//               articlePanels.push(createPanel(articles[i]));
-//           }
-//           // once we have all of the HTML for the articles stored in our articlePanels array, append them to the articlePanels container
-//           articleContainer.append(articlePanels);
-//       }
-
-//       function createPanel(article) {
-//           // this function takes in a single JSON object for an article/headline
-//           // it constructs a jQuery element containing all of the formatted HTML for the article panel
-//           var panel =
-//           $(["<div class='panel panel-default'>",
-//             "<div class='panel-heading'>",
-//             "<h4>",
-//             article.headline,
-//             "</h4>",
-//             article.url,
-//             "</div>",
-//             "<div class='panel-body'>",
-//             "<a class='readLink btn btn-secondary' href='"+article.url+"' target='_blank' rel='noopener'>Read Article",
-//             "</a>",
-//             "<a class='btn btn-success save'>",
-//             "Save Article",
-//             "</a>",
-//             "</div>",
-//             "</div>"
-//       ].join(""));
-//       // we attach the article's id to the jQuery element & will use this when trying to figure out which article the user wants to save
-//     panel.data("_id", article._id);
-//     // return the constructed panel jQuery element
-//     return panel;
-//   }
-//   function renderEmpty() {
-//       // this function renders some HTML to the page saying we don't have any articles to view
-//       var emptyAlert =
-//       $(["<div class='alert alert-warning text-center'>",
-//         "<h4>Uh Oh. Looks like we don't have any new articles.</h4>",
-//         "</div>",
-//         "<div class='panel panel-default'>",
-//         "<div class='panel-heading text-center'>",
-//         "<h3>What Would You Like To Do?</h3>",
-//         "</div>",
-//         "<div class='panel-body text-center'>",
-//         "<h4><a class='scrape-new'>Try Scraping New Articles</a></h4>",
-//         "<h4><a href='/saved'>Go to Saved Articles</a></h4>",
-//         "</div>",
-//         "</div>"
-//   ].join(""));
-//   // appending this data to the page
-//   articleContainer.append(emptyAlert);
-// }
-
-// function handleArticleSave() {
-//   // this function is triggered when the user wants to save an article
-//   var articleToSave = $(this).parents(".panel").data();
-//   articleToSave.saved = true;
-//   // using a patch method to be semantic since this updates an existing record in our collection
-//   $.ajax({
-//       method: "PATCH",
-//       url: "/api/headlines",
-//       data: articleToSave
-//   })
-//   .then(function(data) {
-//       // if success, mongoose will send back an object with a key of "ok" (value of 1 which equals true)
-//       if (data.ok) {
-//           // run the initPage function again to reload the list of articles
-//           initPage();
-//       }
-//   });
-// }
-
-// function handleArticleScrape() {
-//   // this function handles the user clicking any "scrape new article" buttons
-//   $.get("/api/fetch")
-//     .then(function(data) {
-//         // if scrape of NYTIMES successful & compared the articles to the ones in our collection, re-render articles to the page
-//         // and let the user know how many unique articles we were able to save
-//         initPage();
-//         bootbox.alert("<h3 class='text-center m-top-80'>" + data.message + "<h3>");
-//     });
-//   }
-// });
-  
--- server.js
+++ server.js
@@ -15,13 +15,15 @@ var cheerio = require("cheerio");
 //====================================================
 var db = require("./models");
 
+// Initiate express
+//====================================================
+var app = express();
+
 // Port
 //====================================================
 var PORT = process.env.PORT || 3000;
 
-// Initiate express
-//====================================================
-var app = express();
+
 
 // Express router
 //====================================================
@@ -38,17 +40,22 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-// Middleware
-//====================================================
-app.use(router);
+// Configure middleware
 
 // Use morgan logger for logging requests
 app.use(logger("dev"));
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+
 // Make public a static folder
 app.use(express.static("public"));
+
+// Parse request body as JSON
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Handlebars views
+app.engine("handlebars", expressHandlebars({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
 
 
 // If deployed, use deployed db else use local mongoHeadlines db
@@ -57,7 +64,7 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 
 // Connect mongoose
 //====================================================
-mongoose.connect(MONGODB_URI, function(err) {
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, function(err) {
     if (err) {
         console.log(err);
     }
@@ -67,97 +74,125 @@ mongoose.connect(MONGODB_URI, function(err) {
 });
 
 // Routes
-//====================================================
 
 // A GET route for scraping the website
 app.get("/scrape", function(req, res) {
-    // First, we grab the body of the html with axios
-    axios.get("https://www.nytimes.com/").then(function(response) {
-      // Then, we load that into cheerio and save it to $ for a shorthand selector
-      var $ = cheerio.load(response.data);
-  
-      // Now, we grab every h2 within an article tag, and do the following:
-      $("article h2").each(function(i, element) {
-        // Save an empty result object
-        var result = {};
-  
-        // Add the text and href of every link, and save them as properties of the result object
-        result.title = $(this)
-          .children("a")
-          .text();
-        result.link = $(this)
-          .children("a")
-          .attr("href");
-  
-        // Create a new Article using the `result` object built from scraping
-        db.Article.create(result)
-          .then(function(dbArticle) {
-            // View the added result in the console
-            console.log(dbArticle);
-          })
-          .catch(function(err) {
-            // If an error occurred, log it
-            console.log(err);
-          });
-      });
-  
-      // Send a message to the client
-      res.send("Scrape Complete");
+  // First, we grab the body of the html with axios
+  axios.get("https://www.nytimes.com/").then(function(response) {
+    
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+    // Save an empty result object
+    var result = {};
+    
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("div.story-body").each(function(i, element) {
+      
+      //variables to store data
+      var link = $(element).find("a").attr("href");
+			var title = $(element).find("h2.headline").text().trim();
+			var summary = $(element).find("p.summary").text().trim();
+			// var img = $(element).parent().find("figure.media").find("img").attr("src");
+      
+      //save data as property of result object
+      result.link = link;
+      result.title = title;
+      //if there is a summary
+			if (summary) {
+        //save as property of result object
+				result.summary = summary;
+			};
+
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(result)
+        //assigning a variable to hold the new article
+        var entry = result;
+        Article.find({title: result.title}, function(err, data) {
+          if (data.length === 0) {
+            entry.save(function(err, data) {
+              if (err) throw err;
+            });
+          }
+        }
+        .then(function(dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          console.log(err);
+        })
+      );
     });
+    res.send("Scrape Complete");
+  });
 });
-  
+
 // Route for getting all Articles from the db
 app.get("/articles", function(req, res) {
   // Grab every document in the Articles collection
-    db.Article.find({})
-      .then(function(dbArticle) {
-        // If we were able to successfully find Articles, send them back to the client
-        res.json(dbArticle);
-      })
-      .catch(function(err) {
-        // If an error occurred, send it to the client
-        res.json(err);
-      });
-});
-  
-// Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function(req, res) {
-  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-    db.Article.findOne({ _id: req.params.id })
-      // ..and populate all of the notes associated with it
-      .populate("note")
-      .then(function(dbArticle) {
-        // If we were able to successfully find an Article with the given id, send it back to the client
-        res.json(dbArticle);
-      })
-      .catch(function(err) {
-        // If an error occurred, send it to the client
-        res.json(err);
-      });
-});
-  
-// Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
-    // Create a new note and pass the req.body to the entry
-    db.Note.create(req.body)
-      .then(function(dbNote) {
-        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-      })
-      .then(function(dbArticle) {
-        // If we were able to successfully update an Article, send it back to the client
-        res.json(dbArticle);
-      })
-      .catch(function(err) {
-        // If an error occurred, send it to the client
-        res.json(err);
+  db.Article.find({})
+    .then(function(dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
     });
 });
 
-// Listener
-//====================================================
-app.listen(PORT, function() {
-    console.log("Listening on port: " + PORT);
+// Route for getting all saved articles from db
+app.get("/saved", function(req, res) {
+	Article.find({issaved: true}, null, {sort: {created: -1}}, function(err, data) {
+    //if there are no articles in the array 
+    if(data.length === 0) {
+      //render alert
+			res.render("placeholder", {message: "You have not saved any articles yet. Try to save some delicious news by simply clicking \"Save Article\"!"});
+		}
+    else { //otherwise
+      //render saved articles
+			res.render("saved", {saved: data});
+		}
+	});
 });
+
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  db.Article.findOne({ _id: req.params.id })
+    // ..and populate all of the notes associated with it
+    .populate("note")
+    .then(function(dbArticle) {
+      // If we were able to successfully find an Article with the given id, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function(req, res) {
+  // Create a new note and pass the req.body to the entry
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
+
